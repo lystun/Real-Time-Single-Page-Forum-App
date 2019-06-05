27af01ff8a6367691f82b6bcf1b0b54efaec1eb4
Lystun <okegbemiolatunde@gmail.com>
2019-06-05 21:09:25 +0100

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,15 @@ Route::apiResource('category', 'CategoryController');
 Route::apiResource('question/{question}/reply', 'ReplyController');
 
 
+Route::post('like/{reply}', 'LikeController@likeIt');
+Route::delete('like/{reply}', 'LikeController@unLikeIt');
+
+
+Route::group(['prefix' => 'auth'], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
