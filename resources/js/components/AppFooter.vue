ad7060c696c3a4e6c54aefb00455db7199bb4a98
Lystun <okegbemiolatunde@gmail.com>
2019-06-06 17:05:08 +0100

    Vuetify, Vue Router and initial Component

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <v-footer class="pa-3">
+
+        <p>Lystun Real Time SPA</p>
+
+        <v-spacer></v-spacer>
+        <div>&copy; {{ new Date().getFullYear() }}</div>
+
+    </v-footer>
+
+</template>
+
+
+<script>
+    export default {
+        name: "AppFooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
